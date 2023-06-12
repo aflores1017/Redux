03d672576d197303cbae222374b9209bd956c622
Angelo <aflores@senecafoundation.org>
2023-06-12 09:39:12 -0500

    change colors after each button is pressed

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -14,11 +14,18 @@ export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
+  const [backgroundColor, setBackgroundColor] = useState('#FFFFFF'); // Add this line
 
   const incrementValue = Number(incrementAmount) || 0;
 
+  const updateBackgroundColor = () => {
+    const colors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF'];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    setBackgroundColor(colors[randomIndex]);
+  };
+
   return (
-    <div>
+    <div style={{ backgroundColor }}> {/* Add this line */}
       <div className={styles.row}>
         <button
           className={styles.button}
@@ -45,19 +52,28 @@ export function Counter() {
         />
         <button
           className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
+          onClick={() => {
+            dispatch(incrementByAmount(incrementValue));
+            updateBackgroundColor(); // Add this line
+          }}
         >
           Add Amount
         </button>
         <button
           className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
+          onClick={() => {
+            dispatch(incrementAsync(incrementValue));
+            updateBackgroundColor(); // Add this line
+          }}
         >
           Add Async
         </button>
         <button
           className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
+          onClick={() => {
+            dispatch(incrementIfOdd(incrementValue));
+            updateBackgroundColor(); // Add this line
+          }}
         >
           Add If Odd
         </button>
@@ -65,3 +81,4 @@ export function Counter() {
     </div>
   );
 }
+
